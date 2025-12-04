87d4a22b9b572ae1e84d3bc865e02d7980b78ce6
JasTech <linusgodremark@gmail.com>
2025-12-04 06:49:47 -0400

    modified:   src/App.jsx 	new file:   src/assets/image/img26.mov 	new file:   src/components/Hero5.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import Hero1 from "./components/Hero1.jsx";
 import Hero2 from "./components/Hero2.jsx";
+import Hero5 from "./components/Hero5.jsx";
 import Hero3 from "./components/Hero3.jsx";
 import Hero4 from "./components/Hero4.jsx";
 import Footer from "./components/Footer.jsx";
@@ -19,6 +20,7 @@ function App() {
       <Hero />
       <Hero1 />
       <Hero2 />
+      <Hero5 />
       <Hero3 />
       <Hero4 />
       <Footer />
---
+++ src/components/Hero5.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import img26 from "../assets/image/img26.mov";
+
+const Hero5 = () => {
+  return (
+    <div className="relative w-full md:h-screen overflow-hidden md:p-20 flex flex-col md:flex-row items-center gap-6 md:gap-20">
+      <video
+        src={img26}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full md:w-1/2 rounded-lg object-cover max-h-[800px] p-5 md:max-h-[800px]"
+      ></video>
+
+      <div className="text-center md:text-left px-4 md:px-0 md:w-1/2">
+        <p className="text-red-400 mb-2">/06</p>
+        <h2 className="font-bold text-2xl md:text-4xl mb-4">
+          Customized Studio Rental
+        </h2>
+        <p className="text-sm md:text-base leading-relaxed">
+          We offer spaces to content creators and brands, customized according
+          to their needs for podcasts and content creation.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero5;
